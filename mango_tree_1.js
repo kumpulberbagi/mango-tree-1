b270cb1421aaf36867f0373946136d56d82fa4cc
name": null, &   "name": null <pair+dev.wind@devbootcamp.com>
2016-09-28 09:50:10 +0700

    Template

---
+++ mango_tree_1.js
@@ -0,0 +1,32 @@
+"use strict"
+
+class MangoTree {
+  
+  // Initialize a new MangoTree
+  constructor() {
+  }
+
+  // Get current states here
+
+  // Grow the tree
+  grow() {
+  }
+
+  // Produce some mangoes
+  produceMangoes() {
+  }
+
+  // Get some fruits
+  harvest() {
+  }
+}
+
+class Mango {
+  // Produce a mango
+  constructor() {
+  }
+}
+
+function getRandomNumber() {
+}
+
