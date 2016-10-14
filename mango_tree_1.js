65d67972d8b66387f1eae478834c2ad747fd5894
yonisetiawan <yonisetiawan1990@gmail.com>
2016-10-14 10:25:54 +0700

    manggo tree  1 done

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,32 +1,53 @@
 "use strict"
 
+function acak(){
+  var random = Math.ceil((Math.random())*20)
+  return random
+}
+function acakTinggi(){
+  var random = Math.ceil((Math.random())*2)/4
+  return random
+}
+
 class MangoTree {
-  
-  // Initialize a new MangoTree
+
   constructor() {
+    this._maxAge = acak()
+    this._age = 0
+    this._height = acakTinggi()
+    this._fruits = acak()
+    this._harvested = acak()
+    this._alive = true;
   }
 
-  // Get current states here
-
-  // Grow the tree
   grow() {
+
+    this._age +=1
+    while(!this._height){
+        this._height
+    }
+    if(this._age>= this._maxAge){
+      this._alive = false
+    }
   }
 
-  // Produce some mangoes
-  produceMangoes() {
-  }
 
-  // Get some fruits
+
   harvest() {
+      if(this._harvested > this.fruits){
+        console.log("Pohon Sudah Tidak Menghasilkan Buah ");
+      }else{
+        this._harvested = acak()
+        console.log(`Menghasilkan ${this._harvested} Buah`);
+      }
   }
 }
 
-class Mango {
-  // Produce a mango
-  constructor() {
-  }
-}
 
-function getRandomNumber() {
-}
-
+var pohon = new MangoTree()
+console.log(`Pohon Mangga Mulai Tumbuh`);
+do{
+  pohon.grow()
+  console.log(`[Tahun ${pohon._age}] Tinggi Pohon = ${pohon._height += acakTinggi()} Meter | Menghasilkan Buah = ${pohon._harvested + acak()}`);
+} while (pohon._alive != false)
+console.log("Pohon Mati");
